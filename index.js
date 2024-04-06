09a3f94e29ffe090ce29fb45a3cba31ded79d372
Amro Atique <amromatique@gmail.com>
2024-04-06 16:40:37 -0400

    changed index

--- index.js
+++ index.js
@@ -242,11 +242,6 @@ app.post('/find', (req, res) => {
     });
 });
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World!');
-});
 
 server.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}/`);
